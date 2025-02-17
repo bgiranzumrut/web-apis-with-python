34de221cfe436dba10615dcc0d9c9afac1c0fec0
bgiranzumrut <bgiranzumrut@academic.rrc.ca>
2025-02-17 13:51:45 -0600

    Updated /greet route to return JSON response

--- app.py
+++ app.py
@@ -14,4 +14,9 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+    response = {"data": "Hello, World!"} #dictionary response
+    return jsonify(response) #returning the response as a json
+
+    if __name__ == "__main__":
+        app.run(debug=True)
+
